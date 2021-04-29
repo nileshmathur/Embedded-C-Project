578fc39f43b37b3e13ccfb648069fe9bda9a7377
Nilesh Mathur <64744992+nileshmathur@users.noreply.github.com>
2021-04-30 00:53:52 +0530

    Create ledstat.c

---
+++ src/ledstat.c
@@ -0,0 +1,18 @@
+#include<avr/io.h>
+#include "ledstat.h"
+/**
+ * @brief Change the state of the LED Pin according to the value of state
+ * 
+ * @param state Pin level to which the LED Pin should be set
+ */
+void ledstat(uint8_t state)
+{
+	LED_PORT = (state << LED_PIN);
+}
+void InitLED(void)
+{
+	DDRB|=(1<<PB0);
+    DDRD&=~(1<<PD0);
+    PORTD|=(1<<PD0);
+    PORTD|=(1<<PD1);
+}
